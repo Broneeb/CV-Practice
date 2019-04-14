66c8b933e24afaef27d619b86b5a64d4a5284c79
noorahmedds <noorahmedds@outlook.com>
2019-04-14 22:41:27 +0500

    Added SIFT

---
+++ Pract/SIFT Implementation/SIFT.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2 as cv
+
+def main():
+    """SIFT Implementation: Based on https://www.cs.ubc.ca/~lowe/papers/ijcv04.pdf and http://aishack.in/tutorials/sift-scale-invariant-feature-transform-introduction/"""
+    
+    # READ PAPER:
+    # NOTES:
+
+if __name__ == "__main__":
+    main()
